afda26aa278ee51b4c8081995be131640ce46d7a
huangqi <mail_huangqi@163.com>
2025-04-06 13:33:11 +0800

    smart_point 学习

--- exercises/smart_pointers/arc1.rs
+++ exercises/smart_pointers/arc1.rs
@@ -23,23 +23,26 @@
 
 // I AM NOT DONE
 
+
 #![forbid(unused_imports)] // Do not change this, (or the next) line.
 use std::sync::Arc;
 use std::thread;
 
 fn main() {
+    // Fix the typo in `collect()`
     let numbers: Vec<_> = (0..100u32).collect();
-    let shared_numbers = // TODO
+    let shared_numbers = Arc::new(numbers); // Correctly shared with Arc
     let mut joinhandles = Vec::new();
 
     for offset in 0..8 {
-        let child_numbers = // TODO
+        let child_numbers = Arc::clone(&shared_numbers); // Clone the Arc for thread safety
         joinhandles.push(thread::spawn(move || {
             let sum: u32 = child_numbers.iter().filter(|&&n| n % 8 == offset).sum();
             println!("Sum of offset {} is {}", offset, sum);
         }));
     }
-    for handle in joinhandles.into_iter() {
+
+    for handle in joinhandles {
         handle.join().unwrap();
     }
 }
